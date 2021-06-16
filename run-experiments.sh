75d42cc2347b6280a6acdf89a60c658e1d5fd108
Yisu Wang <remywang@dragon.cs.washington.edu>
2021-06-16 11:08:35 -0700

    Automate bigdatalog experiments.

--- run-experiments.sh
+++ run-experiments.sh
@@ -1,13 +1,33 @@
-Datasets="wiki"
+Datasets="twitter"
 # Datasets="twitter epinions wiki"
 Programs="reach"
-# Programs="tc cc sssp"
+# Programs="reach cc sssp"
+
+# args: prog, query, data
+function runbd ()
+{
+	./bin/run-example datalog.Experiments --program=99 --file=redir_$1.txt --queryform="$2" --baserelation_arc=../data/$3/$3$Sfx.csv 2> log 
+	return 0
+}
+
+function qry ()
+{
+	case "$1" in
+		"reach") Query="reach(A)."; Sfx="" ;;
+		"cc") Query="cc(X,Y)."; Sfx="" ;;
+		"sssp") Query="sssp(X,Y)."; Sfx="-w" ;;
+		*) echo "bad program";;
+	esac
+	return 0
+}
 
 for P in $Programs
 do
 	for D in $Datasets
 		do
-			# ./bin/run-example datalog.Experiments --program=99 --file=redir_$P-opt.txt --queryform="reach(A)." --baserelation_arc=../data/$D.csv 2> log &&
-			./bin/run-example datalog.Experiments --program=99 --file=redir_$P.txt --queryform="reach(A)." --baserelation_arc=../data/$D/$D.csv 2> log
+			echo " BENCHMARKING $P on $D" ;
+			qry $P &&
+			runbd $P-opt $Query $D # &&
+			# runbd $P $Query $D 
 		done
 done
